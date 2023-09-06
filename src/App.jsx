7b1cd3bcbd1327d2fb7a005f9f88c2a4d5eea9d9
Empalgentong <saihan507@gmail.com>
2023-09-06 12:36:21 +0700

    mengisi x dan o bergantian

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Square({ value, onSquareClick }) {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
-export default App
+function Board() {
+  const [square, setSquare] = useState(Array(9).fill(null));
+  const [xIsNext, setxIsNext] = useState(true);
+
+  const handleClick = (index) => {
+    if (square[index]) {
+      return;
+    }
+
+    const newSquare = square.slice();
+
+    newSquare[index] = xIsNext ? "X" : "O";
+
+    setSquare(newSquare);
+    setxIsNext(!xIsNext);
+  };
+
+  return (
+    <div className="board">
+      <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+      <Square value={square[2]} onSquareClick={() => handleClick(2)} />
+      <Square value={square[3]} onSquareClick={() => handleClick(3)} />
+      <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+      <Square value={square[5]} onSquareClick={() => handleClick(5)} />
+      <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+      <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+      <Square value={square[8]} onSquareClick={() => handleClick(8)} />
+    </div>
+  );
+}
+
+export default Board;
